288f6342b7ea657bace34cf2a5ef02ada073a00e
Maria Jesus Bellido <mariajbellido@gmail.com>
2021-02-16 19:24:24 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+// Step functionality 
+
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next"); 
+const circles = document.querySelectorAll(".circle");
+
+let currentActive = 1; // First step = 1 
+
+next.addEventListener("click", () => {
+  currentActive++
+
+  if(currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+
+  update()
+  
+})
+
+prev.addEventListener("click", () => {
+  currentActive--
+
+  if(currentActive < 1) {
+    currentActive = 1;
+  }
+
+  update()
+})
+
+
+function update() {
+  circles.forEach((circle, index) => {
+
+      if (index < currentActive) {
+        circle.classList.add("active");
+      } else {
+        circle.classList.remove("active");
+      }
+
+  })
+
+  const actives = document.querySelectorAll(".active") 
+
+
+progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 +'%';
+
+// -1 to avoid exceeding the correct progress bar spacing 
+// Keeping it as % only would make it to overtake part of the next step allowed space.
+
+if (currentActive === 1) {
+  prev.disabled = true; 
+} else if (currentActive === circles.length) {
+  next.disabled = true; 
+} else {
+  prev.disabled = false; 
+  next.disabled = false; 
+}
+}
+
+
+
